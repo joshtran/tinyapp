619f9dda5cd715b872448fd1f9e9a55a3db59ddf
Joshua Tran <joshuatrain.com>
2017-01-18 00:29:23 +0000

    Taking submitted URL, shortens, and redirects.

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub Tiny Aoo
+
+## Problem Statement
+
+Given full URL, create shortened URL. Shortened URL redirects to full URL
+
+## Expected Usage
+
+URL shortening
---
+++ express_server.js
@@ -0,0 +1,66 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  let uniqueURL = Math.random().toString(36).substr(2, 6);
+  return uniqueURL;
+}
+
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls/create", (req, res) => {
+  let shortenedURL = generateRandomString();
+  urlDatabase[shortenedURL] = req.body.longURL;
+  console.log(`http://localhost:8080/urls/${shortenedURL}`);
+  res.redirect(`/urls/${shortenedURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    fullURL: urlDatabase[req.params.id]
+  };;
+  res.render("urls_show", templateVars);
+});
+
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
