f9d8f9137649875593c23d45d8476a52c482468a
Joshua Tran <joshuatrain.com>
2017-01-19 08:35:51 +0000

    Implementing user-specific functionality.

--- express_server.js
+++ express_server.js
@@ -11,8 +11,10 @@ app.use(cookieParser());
 
 //URL Database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "test-1" : {
+      "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  }
 };
 
 //User Database
@@ -31,7 +33,33 @@ function generateRandomString() {
   return uniqueURL;
 }
 
-//Not CRUD - for user experience
+//Check login status and access current user info
+function checkLogin(user_id) {
+  let currentUser = users[user_id];
+  let currentID = null;
+  let currentEmail = null;
+  let currentURLs = null;
+  let loginStatus = false;
+  if (currentUser) {
+    currentID = currentUser.id;
+    currentEmail = currentUser.email;
+    currentURLs = urlDatabase[currentUser.id];
+    loginStatus = true;
+  };
+
+  let loginInfo = {
+    userID: currentID,
+    urls: currentURLs,
+    userEmail: currentEmail,
+    loggedIn: loginStatus
+  };
+
+  return loginInfo;
+
+}
+
+
+//Create new URL Page
 app.get("/urls/new", (req, res) => {
   let templateVars = {
   username: req.cookies["username"],
@@ -39,6 +67,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+//Registration Page
 app.get("/register", (req, res) => {
   let templateVars = {
   username: req.cookies["username"],
@@ -46,7 +75,7 @@ app.get("/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
-
+//Register new user and add to users object
 app.post("/register", (req, res) => {
   let newID = generateRandomString();
   res.cookie("user_id", newID);
@@ -63,13 +92,12 @@ app.post("/register", (req, res) => {
         "email" : newEmail,
         "password" : newPass
         };
-      // console.log(users);
       res.redirect("/");
     }
   }
-
 });
 
+//Login page
 app.get("/login", (req, res) => {
   let templateVars = {
   username: req.cookies["username"],
@@ -77,23 +105,28 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars);
 });
 
+//Log user into tinyapp
 app.post("/login", (req, res) => {
   let loginEmail = req.body.email;
   let loginPass = req.body.password;
   let notFound = true;
 
   for (key in users) {
+
     let existingEmail = users[key].email;
     let existingPass = users[key].password;
     let existingID = users[key].id;
+
     if (loginEmail === existingEmail) {
       notFound = false;
+
       if (loginPass === existingPass){
         res.cookie("user_id", existingID);
         res.redirect("/");
       } else {
         res.sendStatus(403);
       }
+
     }
   }
 
@@ -103,62 +136,92 @@ app.post("/login", (req, res) => {
 
 });
 
+//Log user out of tinyapp
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
   res.clearCookie("user_id");
   res.redirect("/");
 });
 
-//CRUD - Create
+//Create new short URL and assign to long URL - add to urlDatabase object
 app.post("/urls/create", (req, res) => {
-  let shortenedURL = generateRandomString();
-  urlDatabase[shortenedURL] = req.body.longURL;
-  console.log(`http://localhost:8080/urls/${shortenedURL}`);
-  res.redirect(`/urls/${shortenedURL}`);
+  const shortenedURL = generateRandomString();
+  let currentInfo = checkLogin(req.cookies["user_id"]);
+  let currentLoginStatus = currentInfo.loggedIn;
+
+  if (!currentLoginStatus) {
+    res.redirect("/login");
+  } else {
+      //Add short-long URL pair to current user's associated list of URLs in urlDatabase object
+
+
+      //if user's associated URL list doesn't already exist - create and add new URL
+      for (key in urlDatabase) {
+        if (!urlDatabase[currentInfo.userID]) {
+          urlDatabase[currentInfo.userID] = {};
+          urlDatabase[currentInfo.userID][shortenedURL] = req.body.longURL;
+        } else {
+          //if user's associated URL list already exists - just add new URL
+          urlDatabase[currentInfo.userID][shortenedURL] = req.body.longURL;
+        }
+      }
+      res.redirect(`/urls/${shortenedURL}`);
+  }
+
 });
 
-//CRUD - Delete
+//Delete existing short URL from urlDatabase object
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  let currentInfo = checkLogin(req.cookies["user_id"]);
+  let templateVars = currentInfo;
+  delete currentInfo.urls[req.params.id];
+  res.render("urls_index", templateVars);
 });
 
-//CRUD - Update
+//Modify existing short URL-long URL value pair in urlDatabase object
 app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
+  let currentInfo = checkLogin(req.cookies["user_id"]);
+  currentInfo.urls[req.params.id] = req.body.newURL;
   res.send(`Updated URL to ${req.body.newURL}`);
 });
 
-//CRUD - Read
+//Receive shortURL and redirect to Long URL
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  let longURL = "";
+  for (key in urlDatabase) {
+    if (urlDatabase[key][req.params.shortURL]) {
+      longURL = urlDatabase[key][req.params.shortURL];
+      res.redirect(longURL);
+    }
+  }
+  if (!longURL) {
+    res.sendStatus(400);
+  }
 });
 
+//Index page
 app.get("/urls", (req, res) => {
-  let currentUser = users[req.cookies["user_id"]];
-  let currentEmail = null;
-  if (currentUser) {
-    currentEmail = currentUser.email
-  };
-  let templateVars = {
-    urls: urlDatabase,
-    username: req.cookies["username"],
-    userEmail: currentEmail
-  };
+  let currentInfo = checkLogin(req.cookies["user_id"]);
+  let templateVars = currentInfo;
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    fullURL: urlDatabase[req.params.id],
-    username: req.cookies["username"],
-  };;
-  res.render("urls_show", templateVars);
+  let currentInfo = checkLogin(req.cookies["user_id"]);
+
+  let currentLoginStatus = currentInfo.loggedIn;
+
+  if (!currentLoginStatus) {
+    res.send("not logged in");
+  } else {
+      let templateVars = {
+      shortURL: req.params.id,
+      fullURL: urlDatabase[currentInfo.userID][req.params.id]
+    };
+    res.render("urls_show", templateVars);
+  }
 });
 
-
+//Root and testing pages
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
